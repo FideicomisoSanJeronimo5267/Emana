0b721dcc6b2a5ae68efe936f5fada6cbd0ff77c1
Orlando Callejas <orlando.callejas@odigos.mx>
2026-03-30 03:04:53 -0700

    Feat(Collaborator):Fixing responsive

--- src/app/(pages)/colaboradores/page.tsx
+++ src/app/(pages)/colaboradores/page.tsx
@@ -1,40 +1,43 @@
-import CollaboratorSection from '../../components/collaborator-section';
-import ProjectDetailsBanner from '../../components/project-details-banner';
 import styles from './colaboradores.module.css';
+import CollaboratorSection from './presentation/components/collaborator-section';
+import { collaboratorsImagesdata } from '@/src/core/constants/image-data/collaborators-page';
+const collaborators = [
+    {
+        name: "ONE DEVELOPMENT GROUP",
+        description: "Empresa mexicana dedicada a crear espacios inmobiliarios de nueva generación y experiencias memorables que transforman la forma de vivir, trabajar y convivir. \n\n En cada proyecto, ONE prioriza generar un impacto positivo en residentes, usuarios, visitantes, inversionistas y en la comunidad que rodea cada desarrollo. \n\n Entre sus proyectos más destacados se encuentran SOFIA, Arboleda, El Gran Ancira y GALA en Nuevo León, así como NAYA y NAYAMĀ en el estado de Nayarit, y ALISIA en Jalisco.",
+        image: collaboratorsImagesdata.one_development_group,
+        logo: "/assets/images/collaborators/odg.svg"
+    },
+    {
+        name: "JSA",
+        description: "Taller arquitectónico mexicano liderado por la visión de Javier Sánchez, cuya práctica se basa en procesos colectivos y estrategias de convivencia a distintas escalas. \n\n Su trabajo se ha consolidado a través de proyectos como Pujol, Hotel Carlota y The Cape en Los Cabos, creando espacios que dialogan de manera orgánica con su entorno. \n\n JSA entiende la arquitectura como un ciclo continuo de aprendizaje, investigación y aproximación urbana, donde cada proyecto se convierte en una propuesta de valor para clientes y usuarios.",
+        image: collaboratorsImagesdata.jsa,
+        logo: "/assets/images/collaborators/jsa.svg"
+    },
+    {
+        name: "ESRAWE",
+        description: "Es un taller multidisciplinario de diseño con sede en la Ciudad de México, especializado en mobiliario, interiorismo y soluciones arquitectónicas para proyectos residenciales, culturales y de hospitalidad. \n\n Su trabajo se distingue por una visión sofisticada que combina precisión técnica, sensibilidad estética y una profunda conexión con el contexto. \n\n A través de un proceso meticuloso y colaborativo, el estudio transforma cada proyecto en una experiencia coherente, innovadora y atemporal.",
+        image: collaboratorsImagesdata.esrawe,
+        logo: "/assets/images/collaborators/esrawe.svg"
+    }
+];
 
-export default function ColaboradoresPage() {
+export default function CollaboratorsPage() {
     return (
         <main className={styles.main}>
 
-            <CollaboratorSection
-                title="One Development Group"
-                description="Empresa mexicana dedicada a crear espacios inmobiliarios de nueva generación y experiencias memorables que transforman la forma de vivir, trabajar y convivir. En cada proyecto, ONE prioriza generar un impacto positivo en residentes, usuarios, visitantes, inversionistas y en la comunidad que rodea cada desarrollo. Entre sus proyectos más destacados se encuentran SOFIA, Arboleda, El Gran Ancira y GALA en Nuevo León, así como NAYA y NAYAMĀ en el estado de Nayarit, y ALISIA en Jalisco."
-                imageAlt="Imagen: 699_Retail_HQ.png"
-                logoAlt="Logo ONE DEVELOPMENT GROUP"
-                logoWidth="189px"
-                logoHeight="37px"
-            />
-
-            <CollaboratorSection
-                title="JSa"
-                description="Taller arquitectónico mexicano liderado por la visión de Javier Sánchez, cuya práctica se basa en procesos colectivos y estrategias de convivencia a distintas escalas. Su trabajo se ha consolidado a través de proyectos como Pujol, Hotel Carlota y The Cape en Los Cabos, creando espacios que dialogan de manera orgánica con su entorno. JSA entiende la arquitectura como un ciclo continuo de aprendizaje, investigación y aproximación urbana, donde cada proyecto se convierte en una propuesta de valor para clientes y usuarios."
-                imageAlt="Imagen: JSa_CEMEFI_1456+(Footer).jpg"
-                logoAlt="Logo JSa (ksDG2I)"
-                logoWidth="93px"
-                logoHeight="90px"
-                reversed
-            />
-
-            <CollaboratorSection
-                title="ESRAWE"
-                description={"Es un taller multidisciplinario de diseño con sede en la Ciudad de México especializado en mobiliario, interiorismo y arquitectura, de la cual ha creado un sello personal que trasciende a través del tiempo.\n\nSu concepción de diseño persigue una red interactiva como su principal motor de creatividad estética, explorando y revalorizando la manufactura y los procesos manuales.\n\nA través de un proceso metódico y colaborativo el estudio de Esrawe ha participado en una infinidad de proyectos y en el diseño de diversas tipologías."}
-                imageAlt="Imagen: JUNE_04.png"
-                logoAlt="Logo ESRAWE"
-                logoWidth="188px"
-                logoHeight="28px"
-            />
-
-            <ProjectDetailsBanner />
+            <div className={styles.collaboratorsList}>
+                {collaborators.map((collab, index) => (
+                    <CollaboratorSection
+                        key={index}
+                        name={collab.name}
+                        description={collab.description}
+                        image={collab.image}
+                        isReversed={index % 2 == 0}
+                        logo={collab.logo}
+                    />
+                ))}
+            </div>
         </main>
-    );
+    )
 }
---
+++ src/app/(pages)/colaboradores/presentation/components/collaborator-section/index.tsx
@@ -0,0 +1,48 @@
+import Image from 'next/image';
+import styles from './collaborator-section.module.css';
+
+interface CollaboratorProps {
+    name: string;
+    description: string;
+    image: {
+        src: string;
+        alt: string;
+        blurData: string;
+    };
+    isReversed?: boolean;
+    logo: string;
+}
+
+export default function CollaboratorSection({ name, description, image, isReversed = false, logo }: CollaboratorProps) {
+    return (
+        <section className={`${styles.collaboratorSection} ${isReversed ? styles.reversed : ''}`}>
+            <div className={styles.collaboratorInfo}>
+                <h3 className={styles.collaboratorName}>{name}</h3>
+                <p className={styles.collaboratorDescription}>{description}</p>
+
+                <div className={styles.logoPlaceholder}>
+                    <Image
+                        src={logo}
+                        alt={name}
+                        fill
+                        objectFit='contain'
+                        className={styles.logo}
+                    />
+
+                </div>
+            </div>
+            <div className={styles.collaboratorImageWrapper}>
+                <Image
+                    src={image.src}
+                    alt={image.alt}
+                    fill
+                    className={styles.collaboratorImage}
+                    placeholder={"blur"}
+                    blurDataURL={image.blurData}
+                    objectFit='cover'
+                />
+            </div>
+        </section>
+    );
+}
+
