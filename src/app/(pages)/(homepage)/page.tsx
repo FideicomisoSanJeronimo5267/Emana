335f2e44d13a74a68d198fd8138f65254884029f
Carlos Catalan <dev@odigos.mx>
2026-03-30 02:13:43 -0700

    Feat(Homepage - Residences page): updating page structure

--- src/app/(pages)/(homepage)/page.tsx
+++ src/app/(pages)/(homepage)/page.tsx
@@ -9,6 +9,9 @@ import coverpage from '@assets/images/homepage/cover-image.png';
 
 // Components
 import { AmenitiesSection, FeaturesSection, LocationSection } from './presentation/components';
+import { BrochureSection, UnitsAvailable } from '../residencias/presentation/components';
+import { AppointmentSection } from '../../components';
+import { residencesImagesdata } from '@/src/core/constants/image-data/residences-page';
 
 export default function Home() {
   return (
@@ -21,6 +24,25 @@ export default function Home() {
       <div className={styles.main__axis__divisor} />
       <LocationSection />
       <AmenitiesSection />
+      <UnitsAvailable
+        title='Desde $8.7 MDP*'
+        button={{
+          title: 'Agenda una cita'
+        }}
+        headerDescription='Departamentos y villas de lujo'
+        footerDescription='*Sujeto a disponibilidad y cambios sin previo aviso.'
+      />
+      <div className={styles.main__axis__divisor} />
+      <BrochureSection
+        title='Conoce más sobre EMANA'
+        description='Descubre cómo el diseño, la ubicación y las amenidades del proyecto se unen para crear una experiencia residencial única.'
+      />
+      <AppointmentSection
+        title={'Vive la experiencia EMANA'}
+        description={'Visita nuestro showroom y conoce el futuro de tu inversión.'}
+        coverImage={residencesImagesdata.appointmentBG.src}
+        blurDataURL={residencesImagesdata.appointmentBG.blurData}
+      />
     </main>
   );
 }
